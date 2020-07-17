0c903503cb4c9813ae88b66f749f8aedd7e0542f
desobob11 <67239552+desobob11@users.noreply.github.com>
2020-07-17 12:38:16 -0600

    change

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,47 @@
 
 public class Course {
+	private String faculty;
+	private int buyPrice;
+	private int sellPrice;
+	private int withdrawPrice;
+	private boolean ownedStatus;
+	private int courseLevel;
+	private int courseID;
+	
+		public String getFaculty() {
+		return faculty;
+		}
+	
+		public int getBuyPrice() {
+			return buyPrice;
+		}
 
+		public int getSellPrice() {
+			return sellPrice;
+		}
+		
+		public int getWithdrawPrice() {
+			return withdrawPrice;
+		}
+		
+		public boolean getOwnedStatus() {
+			return ownedStatus;
+		}
+		
+		public int getCourseLevel() {
+			return courseLevel;
+		}
+		
+		public int getCourseID() {
+			return courseID;
+		}
+		
+		public boolean setOwnedStatus(boolean check) {
+			this.ownedStatus = check;
+			return ownedStatus;
+		}
+		
+		
+		
 }
+
