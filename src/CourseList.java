0c6ae4e895ac883d97c1128c58d87ecd7330a9f6
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-19 17:13:52 -0600

    Update CourseList.java

--- src/CourseList.java
+++ src/CourseList.java
@@ -1,4 +1,36 @@
-
+import java.util.ArrayList;
 
 public class CourseList {
+	static private ArrayList<Course> courseList;
+	private ArrayList<Course> coursesOwned;
+	int boardSize = 20;
+	
+	public boolean inCourseList(Course aCourse) {
+		return (courseList.contains(aCourse) ? true: false);
+	}
+	
+	public boolean inCoursesOwned(Course aCourse) {
+		return (coursesOwned.contains(aCourse) ? true: false);
+	}
+	
+	public int addToCoursesOwned(Course aCourse) {
+		if (inCourseList(aCourse)) {
+			if (!inCoursesOwned(aCourse)) {
+				coursesOwned.add(aCourse);
+				return 1;
+			}
+			return -2;
+		}
+		return -1;
+	}
+	
+	public int removeFromCoursesOwned(Course aCourse) {
+		if (inCourseList(aCourse)) {
+			if (inCoursesOwned(aCourse)) {
+				coursesOwned.remove(aCourse);
+			}
+			return -2;
+		}
+		return -1;
+	}
 }
