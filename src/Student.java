d70825f6bfea7562b92cd3a6b1446e5b14ebccb1
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-19 17:13:54 -0600

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Student {
 	private int playerNumber;
-	private double playerMoney;
+	private int playerMoney;
 	private int playerPosition = 0;
 	private ArrayList<Course> coursesOwned;
 	private boolean ownsFacultyOne = false;
@@ -14,7 +14,7 @@ public class Student {
 		
 	}
 	
-	public Student(int playerNumber, double playerMoney)
+	public Student(int playerNumber, int playerMoney)
 	{
 		if (playerNumber > 0 && playerNumber < 5) {	
 			this.playerNumber = playerNumber;
@@ -24,15 +24,15 @@ public class Student {
 		}
 	}
 	
-	public int getPlayerNumber(int playerNumber) {
+	public int getPlayerNumber() {
 		return playerNumber;
 	}
 	
-	public double getPlayerMoney(int money) {
+	public int getPlayerMoney() {
 		return playerMoney;
 	}
 	
-	public int getPlayerPosition(int money) {
+	public int getPlayerPosition() {
 		return playerPosition;
 	}
 	
@@ -44,4 +44,55 @@ public class Student {
 		return (coursesOwned.contains(aCourse) ? true : false);
 	}
 	
+	private void addCourse(Course aCourse) {
+		this.coursesOwned.add(aCourse);
+	}
+	
+	private boolean removeCourse(Course aCourse) {
+		return this.coursesOwned.remove(aCourse);
+	}
+	
+	private int withdrawMoney(int money) {
+		if (money <= playerMoney) {
+			playerMoney -= money;
+			return 1;
+		}
+		return -1;
+	}
+	
+	private int depositMoney(int money) {
+		if (money >= 0) {
+			playerMoney += money;
+			return 1;
+		}
+		return -1;
+	}
+	
+	public int purchaseCourse(Course aCourse) {
+		if (!aCourse.getOwnedStatus()) {
+			if (withdrawMoney(aCourse.getBuyPrice()) == 1) {
+				aCourse.setOwnedStatus(true);
+				aCourse.setOwner(this);
+				this.addCourse(aCourse);
+				return 1;
+			}
+			return -2;
+		}
+		return -1;
+	}
+	
+	public int sellCourse(Course aCourse) {
+		if (this.doesStudentOwnCourse(aCourse)) {
+			aCourse.setOwnedStatus(false);
+			aCourse.setOwner(null);
+			depositMoney(aCourse.getSellPrice());
+			return (removeCourse(aCourse) ? -2 : 0);
+		}
+		return -1;
+	}
+	
+	public void moveForward(int spaces, int boardSize) {
+		playerPosition += spaces;
+		playerPosition %= boardSize;
+	}
 }
