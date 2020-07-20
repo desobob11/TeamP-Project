1e453803be34150ec4321e98e5026de2e1371b49
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-20 14:36:52 -0600

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -3,52 +3,62 @@ public class Course extends Tile {
 	private String faculty;
 	private int buyPrice;
 	private int sellPrice;
-	private int withdrawPrice;
+	private int tutorialPrice;
 	private boolean ownedStatus;
 	private int courseLevel;
 	private Student ownedBy = null;
 	
-		public String getFaculty() {
-			return faculty;
-		}
+	public Course(int tileID, String tileName, String faculty, int buyPrice, int sellPrice, int tutorialPrice) {
+		super(tileID, tileName);
+		this.faculty = faculty;
+		this.buyPrice = buyPrice;
+		this.sellPrice = sellPrice;
+		this.tutorialPrice = tutorialPrice;
+		this.ownedStatus = false;
+		this.courseLevel = 1;
+	}
 	
-		public Student getOwner() {
-			return ownedBy;
-		}
-		
-		public Student setOwner(Student that) {
-			ownedBy = that;
-			return ownedBy;
-		}
-		
-		public int getBuyPrice() {
-			return buyPrice;
-		}
+	public String getFaculty() {
+		return faculty;
+	}
 
-		public int getSellPrice() {
-			return sellPrice;
-		}
-		
-		public int getWithdrawPrice() {
-			return withdrawPrice;
-		}
-		
-		public boolean getOwnedStatus() {
-			return ownedStatus;
-		}
-		
-		public int getCourseLevel() {
-			return courseLevel;
-		}
-		
-		public boolean setOwnedStatus(boolean check) {
-			this.ownedStatus = check;
-			return ownedStatus;
-		}
-		
-		public int getTileId() {
-			return this.getTileId();
-		}
+	public Student getOwner() {
+		return ownedBy;
+	}
+	
+	public Student setOwner(Student that) {
+		ownedBy = that;
+		return ownedBy;
+	}
+	
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+
+	public int getSellPrice() {
+		return sellPrice;
+	}
+	
+	public int getTutorialPrice() {
+		return tutorialPrice;
+	}
+	
+	public boolean getOwnedStatus() {
+		return ownedStatus;
+	}
+	
+	public int getCourseLevel() {
+		return courseLevel;
+	}
+	
+	public boolean setOwnedStatus(boolean check) {
+		this.ownedStatus = check;
+		return ownedStatus;
+	}
+	
+	public int getTileId() {
+		return this.getTileId();
+	}
 		
 		
 }
