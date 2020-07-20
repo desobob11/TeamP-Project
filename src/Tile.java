d02a1c2a71b4c78674f8f59a53bccadd1404257f
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-20 14:02:20 -0600

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,9 +1,23 @@
 
 public class Tile {
-	private int tileId;
+	private int tileID;
+	private String tileName;
 	
-	public int getTileId() {
-		return tileId;
+	public Tile() {
+		
+	}
+	
+	public Tile(int tileID, String tileName) {
+		this.tileID = tileID;
+		this.tileName = tileName;
+	}
+	
+	public int getTileID() {
+		return tileID;
+	}
+	
+	public String getTileName() {
+		return tileName;
 	}
 	
 }
