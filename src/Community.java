3ac3ee7787541cb39e38a73bf2ad8392e2505886
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-20 14:02:08 -0600

    Create Community.java

---
+++ src/Community.java
@@ -0,0 +1,10 @@
+
+public class Community extends Tile {
+	String[] communityOptions = {"Scholarship: Earn $500",
+	                             "Student Aid: Everyone earns $100",
+	                             "Move back to Go"};
+	
+	public Community(int position) {
+		super(position, "Community");
+	}
+}
