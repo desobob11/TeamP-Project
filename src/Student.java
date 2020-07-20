d324f2902e6eaf465fbb5add63f6acbf2ccd423d
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-20 14:02:18 -0600

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Student {
 	private int playerNumber;
 	private int playerMoney;
 	private int playerPosition = 0;
 	private ArrayList<Course> coursesOwned;
-	private boolean ownsFacultyOne = false;
-	private boolean ownsFacultyTwo = false;
-	private boolean ownsFacultyThree = false;
-	private boolean ownsFacultyFour = false;
+	private HashMap<String, Boolean> ownsFaculty;
+
+
 	
 	public Student() {
-		
+		initializeOwnsFaculty();
 	}
 	
 	public Student(int playerNumber, int playerMoney)
@@ -22,6 +21,15 @@ public class Student {
 		if (playerMoney > 0) {
 			this.playerMoney = playerMoney;
 		}
+		initializeOwnsFaculty();
+	}
+	
+	public void initializeOwnsFaculty() {
+		ownsFaculty = new HashMap<String, Boolean>();
+		ownsFaculty.put("Arts", false);
+		ownsFaculty.put("Sciences", false);
+		ownsFaculty.put("Business", false);
+		ownsFaculty.put("Engineering", false);
 	}
 	
 	public int getPlayerNumber() {
@@ -52,20 +60,33 @@ public class Student {
 		return this.coursesOwned.remove(aCourse);
 	}
 	
-	private int withdrawMoney(int money) {
+	public int withdrawMoney(int money) {
 		if (money <= playerMoney) {
 			playerMoney -= money;
 			return 1;
 		}
+		else if (!isBankrupt(money)) {
+			return -2;
+		}
 		return -1;
 	}
 	
-	private int depositMoney(int money) {
+	public int depositMoney(int money) {
 		if (money >= 0) {
 			playerMoney += money;
 			return 1;
 		}
 		return -1;
+	}
+	
+	public boolean isBankrupt(int money) {
+		int mortgageValue = 0;
+		
+		for (Course courseOwned: coursesOwned) {
+			mortgageValue += courseOwned.getSellPrice();
+		}
+		
+		return (mortgageValue + playerMoney < money ? true : false);
 	}
 	
 	public int purchaseCourse(Course aCourse) {
@@ -74,6 +95,18 @@ public class Student {
 				aCourse.setOwnedStatus(true);
 				aCourse.setOwner(this);
 				this.addCourse(aCourse);
+				
+				String courseFaculty = aCourse.getFaculty();
+				int facultyCounter = 0;
+				for (Course courseOwned: coursesOwned) {
+					if (courseOwned.getFaculty() == courseFaculty) {
+						facultyCounter++;
+					}
+				}
+				if (facultyCounter == 3) {
+					ownsFaculty.put(courseFaculty, true);
+				}
+				
 				return 1;
 			}
 			return -2;
