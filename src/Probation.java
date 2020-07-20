c0bc9c219dbfad5dff264ae1c51a486a74aa3fba
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-20 15:18:54 -0600

    Update Probation.java

--- src/Probation.java
+++ src/Probation.java
@@ -2,7 +2,7 @@
 public class Probation extends Tile {
 
 
-public Probation(int position) {
-	super(position, "Probation");
+	public Probation(int position) {
+		super(position, "Probation");
 	}
 }
