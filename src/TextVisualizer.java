327029cc6dbd01ec824e327d3bf662a966397c2e
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-21 01:49:44 -0600

    Update TextVisualizer.java

--- src/TextVisualizer.java
+++ src/TextVisualizer.java
@@ -1,4 +1,42 @@
 
 public class TextVisualizer {
-
+	public TextVisualizer() {
+		
+	}
+	
+	public void insufficientMoneyError() {
+		
+	}
+	
+	public void rollDiceMenu() {
+		
+	}
+	
+	public void displayCourseOptions() {
+		
+	}
+	
+	public void displayCourseOwnedMenu(Student ower, Student owner) {
+		
+	}
+	
+	public void displayTutorialPaidScreen(Student ower, Student owner) {
+		
+	}
+	
+	public void displayBankruptcyScreen(Student student) {
+		
+	}
+	
+	public void removePlayerFromUI(Student student) {
+		
+	}
+	
+	public void displayMustMortgageScreen(Student student) {
+		
+	}
+	
+	public Course sellCourseMenu(Student student) {
+		return student.getCoursesOwned().get(0);
+	}
 }
