bf481778146e182fffa28e90279dfea5f443e3ae
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-21 12:52:11 -0600

    Update Chance.java

--- src/Chance.java
+++ src/Chance.java
@@ -1,8 +1,16 @@
 
 public class Chance extends Tile {
-	String[] chanceOptions = {"SU Donation: Pay $100",
-	                          "Reimbursement: Earn $200",
-	                          "Help out a friend: Pay the player next to you $50"};
+	String[] chanceOptions = {
+            "SU Donation: Pay $100",
+            "Grants have been doubled everyone collect 300$",
+            "Your offered a ride from your friend, move to free parking",
+            "Your midterm started 10 minutes ago advance to the nearest bus station",
+            "Help out a friend: Pay the player next to you $50",
+            "Your student loans have caught up with you, pay 250 $",
+            "You withdrew from a course, past the deadline!, pay 150",
+            "You have been caught for cheating, move to academic probation",
+            "You forgot your uni card at home, advance to the nearest bus station, if owned pay double",
+    };
 	
 	public Chance(int position) {
 		super(position, "Chance");
