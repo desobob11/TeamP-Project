d3b5d02ac56ac3d8ddabb4cb211b49b49897c96a
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-22 17:14:41 -0600

    Update Chance.java

--- src/Chance.java
+++ src/Chance.java
@@ -1,15 +1,13 @@
+import java.util.ArrayList;
 
 public class Chance extends Tile {
 	String[] chanceOptions = {
             "SU Donation: Pay $100",
-            "Grants have been doubled everyone collect 300$",
-            "Your offered a ride from your friend, move to free parking",
-            "Your midterm started 10 minutes ago advance to the nearest bus station",
-            "Help out a friend: Pay the player next to you $50",
-            "Your student loans have caught up with you, pay 250 $",
-            "You withdrew from a course, past the deadline!, pay 150",
+            "Grants have been doubled everyone collect $300",
+            "Your offered a ride from your friend, move to the nearest free parking",
+            "Your student loans have caught up with you, pay $250",
+            "You withdrew from a course, past the deadline! Pay $150",
             "You have been caught for cheating, move to academic probation",
-            "You forgot your uni card at home, advance to the nearest bus station, if owned pay double",
     };
 	
 	public Chance(int position) {
@@ -19,6 +17,32 @@ public class Chance extends Tile {
 	public String[] getChanceOptions() {
 		return chanceOptions;
 	}
+
+	public int performChanceOption(int selection, Student studentOn, ArrayList<Student> allStudents, int parking1Pos,
+			Integer parking2Pos, int probationPos) {
+		switch (selection) {
+		case 1:
+			return studentOn.withdrawMoney(100);
+		case 2:
+			for (Student student: allStudents) {
+				student.depositMoney(300);
+			}
+			return 1;
+		case 3:
+			studentOn.moveToClosestParking(parking1Pos, parking2Pos);
+			return 1;
+		case 4:
+			return studentOn.withdrawMoney(250);
+		case 5:
+			return studentOn.withdrawMoney(150);
+		case 6:
+			studentOn.moveToProbation(probationPos);
+			return 1;
+		}
+		return 1;
+	}
+	
+	
 
 
 }
