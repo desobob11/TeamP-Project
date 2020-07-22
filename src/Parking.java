5961e24c8317c02565244172309449fc22721139
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-22 17:14:43 -0600

    Update Parking.java

--- src/Parking.java
+++ src/Parking.java
@@ -1,7 +1,13 @@
 
 public class Parking extends Tile {
-
+	private int parkingCost = 250;
+	
 	public Parking(int position) {
 		super(position, "Parking");
-	}	
+	}
+	
+	public Parking (int position, int parkingCost) {
+		super(position, "Parking");
+		this.parkingCost = parkingCost;
+	}
 }
