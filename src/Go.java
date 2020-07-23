46aa1dfa1004c243a1600507195df3c32cd7c746
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-23 00:54:00 -0600

    Update Go.java

--- src/Go.java
+++ src/Go.java
@@ -1,15 +1,21 @@
 
+/**
+ * This is the Go tile, the simplest child of Tile. It simply gives the student
+ * a fixed amount for landing on it, and, by definition, is always at position 0
+ * 
+ * @author Desmond O'Brien
+ *
+ */
 public class Go extends Tile {
 
 	private int landDeposit = 200;
-	
+
 	public Go() {
 		super(0, "Go");
 	}
-	
-	
+
 	public void depositGoAmmount(Student student) {
 		student.depositMoney(landDeposit);
 	}
-	
+
 }
