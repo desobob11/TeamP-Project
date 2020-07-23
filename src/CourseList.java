08f5435b273a47e7b2fa1cee003d40efa03dcb8b
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-22 22:19:10 -0600

    Update CourseList.java

--- src/CourseList.java
+++ src/CourseList.java
@@ -12,6 +12,13 @@ public class CourseList {
 	int boardSize = 20;
 	
 	public CourseList() {
+		board = new ArrayList<Tile>();
+		courseList = new ArrayList<Course>();
+		coursesOwned = new ArrayList<Course>();
+		chanceTiles = new ArrayList<Chance>();
+		communityTiles = new ArrayList<Community>();
+		parkingTiles = new ArrayList<Parking>();
+		
 		initializeCourses();
 		initializeUnbuyableTiles();
 		ArrayList<Tile> allTiles = new ArrayList<Tile>();
@@ -26,6 +33,13 @@ public class CourseList {
 	
 	public CourseList(ArrayList<Course> courseList) {
 		this.courseList = courseList;
+		board = new ArrayList<Tile>();
+		courseList = new ArrayList<Course>();
+		coursesOwned = new ArrayList<Course>();
+		chanceTiles = new ArrayList<Chance>();
+		communityTiles = new ArrayList<Community>();
+		parkingTiles = new ArrayList<Parking>();
+		
 		initializeUnbuyableTiles();
 		ArrayList<Tile> allTiles = new ArrayList<Tile>();
 		allTiles.addAll(courseList);
@@ -39,40 +53,40 @@ public class CourseList {
 	
 	public void initializeCourses() {
 		Course course1 = new Course(1, "SOCI 201", "Arts", 150, 50, 50);
-        coursesOwned.add(course1);
+        courseList.add(course1);
         
-        Course course2 = new Course(2, "POLI 201", "Arts", 175, 60, 55);
-        coursesOwned.add(course2);
+        Course course2 = new Course(3, "POLI 201", "Arts", 175, 60, 55);
+        courseList.add(course2);
         
-        Course course3 = new Course(3, "PSYC 201", "Arts", 200, 70, 60);
-        coursesOwned.add(course3);
+        Course course3 = new Course(4, "PSYC 201", "Arts", 200, 70, 60);
+        courseList.add(course3);
         
-        Course course4 = new Course(4, "CPSC 231", "Sciences", 250, 80, 65);
-        coursesOwned.add(course4);   
+        Course course4 = new Course(6, "CPSC 231", "Sciences", 250, 80, 65);
+        courseList.add(course4);   
         
-        Course course5 = new Course(5, "CPSC 219", "Sciences", 275, 90, 70);
-        coursesOwned.add(course5);
+        Course course5 = new Course(8, "CPSC 219", "Sciences", 275, 90, 70);
+        courseList.add(course5);
         
-        Course course6 = new Course(6, "CPSC 233", "Sciences", 300, 100, 75);
-        coursesOwned.add(course6);
+        Course course6 = new Course(9, "CPSC 233", "Sciences", 300, 100, 75);
+        courseList.add(course6);
         
-        Course course7 = new Course(7, "STAT 213","Business", 350, 110, 80);
-        coursesOwned.add(course7);
+        Course course7 = new Course(11, "STAT 213","Business", 350, 110, 80);
+        courseList.add(course7);
         
-        Course course8 = new Course(8, "ENMG 301", "Business", 375, 120, 85);
-        coursesOwned.add(course8);
+        Course course8 = new Course(13, "ENMG 301", "Business", 375, 120, 85);
+        courseList.add(course8);
         
-        Course course9 = new Course(9, "MGMT 311", "Business", 400, 130, 90);
-        coursesOwned.add(course9);
+        Course course9 = new Course(14, "MGMT 311", "Business", 400, 130, 90);
+        courseList.add(course9);
         
-        Course course10 = new Course(10, "ENGG 599", "Engineering", 450, 140, 100);
-        coursesOwned.add(course10);
+        Course course10 = new Course(16, "ENGG 599", "Engineering", 450, 140, 100);
+        courseList.add(course10);
         
-        Course course11 = new Course(11, "ENMG 301", "Engineering", 475, 150, 110);
-        coursesOwned.add(course11);
+        Course course11 = new Course(17, "ENMG 301", "Engineering", 475, 150, 110);
+        courseList.add(course11);
         
-        Course course12 = new Course(12, "MGMT 311", "Engineering", 500, 160, 120);
-        coursesOwned.add(course12);
+        Course course12 = new Course(19, "MGMT 311", "Engineering", 500, 160, 120);
+        courseList.add(course12);
 	}
 	
 	public void initializeUnbuyableTiles() {
@@ -90,16 +104,16 @@ public class CourseList {
         Parking parking2 = new Parking(parking2Pos);
 		parkingTiles.add(parking2);
 		
-		Chance chance1 = new Chance(2);
+		Chance chance1 = new Chance(7);
         chanceTiles.add(chance1);
         
-        Community community1 = new Community(7);
+        Community community1 = new Community(2);
         communityTiles.add(community1);
         
-        Chance chance2 = new Chance(13);
+        Chance chance2 = new Chance(18);
         chanceTiles.add(chance2);
         
-        Community community2 = new Community(17);
+        Community community2 = new Community(12);
         communityTiles.add(community2);
 	}
 	
@@ -203,8 +217,8 @@ public class CourseList {
 			for (Tile tile: allTiles) {
 				if (tile.getTileID() == i) {
 					board.add(tile);
+					break;
 				}
-				break;
 			}
 		}
 	}
