7c8892839a154a0da40b24b12e3ea8b51450239e
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-23 00:54:15 -0600

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,23 +1,29 @@
-
+/**
+ * Tile - this represents a piece on the board. There will never be an instance
+ * of Tile itself, but rather one of its many children.
+ * 
+ * @author Desmond O'Brien and Arnuv Mayank
+ *
+ */
 public class Tile {
 	private int tileID;
 	private String tileName;
-	
+
 	public Tile() {
-		
+
 	}
-	
+
 	public Tile(int tileID, String tileName) {
 		this.tileID = tileID;
 		this.tileName = tileName;
 	}
-	
+
 	public int getTileID() {
 		return tileID;
 	}
-	
+
 	public String getTileName() {
 		return tileName;
 	}
-	
+
 }
