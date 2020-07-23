53f461fde66faaf318d99c4bcbff091c8ce3dc4e
arnuvmayankuofc <arnuv.mayank1@ucalgary.ca>
2020-07-23 00:53:53 -0600

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,12 @@
-
+/**
+ * This is the course class, the most important child of Tile. It handles
+ * everything related to course modification, ownership, etc. Student is what
+ * handles buying and selling courses, but Course is still very important for
+ * CourseList to perform its function.
+ * 
+ * @author Arnuv Mayank
+ *
+ */
 public class Course extends Tile {
 	private String faculty;
 	private int buyPrice;
@@ -9,7 +17,7 @@ public class Course extends Tile {
 	private int facultyUpgradeCost;
 	private int initialFacultyUpgradeCost;
 	private Student ownedBy = null;
-	
+
 	public Course(int tileID, String tileName, String faculty, int buyPrice, int sellPrice, int tutorialPrice) {
 		super(tileID, tileName);
 		this.faculty = faculty;
@@ -18,29 +26,26 @@ public class Course extends Tile {
 		this.tutorialPrice = tutorialPrice;
 		this.ownedStatus = false;
 		this.courseLevel = 1;
-		
+
 		initializeUpgradeCost();
 	}
-	
+
 	public void initializeUpgradeCost() {
 		if (this.faculty == "Arts") {
 			this.facultyUpgradeCost = 150;
 			this.initialFacultyUpgradeCost = 150;
-		}
-		else if (this.faculty == "Sciences") {
+		} else if (this.faculty == "Sciences") {
 			this.facultyUpgradeCost = 210;
 			this.initialFacultyUpgradeCost = 210;
-		}
-		else if (this.faculty == "Business") {
+		} else if (this.faculty == "Business") {
 			this.facultyUpgradeCost = 270;
 			this.initialFacultyUpgradeCost = 270;
-		}
-		else if (this.faculty == "Engineering") {
+		} else if (this.faculty == "Engineering") {
 			this.facultyUpgradeCost = 330;
 			this.initialFacultyUpgradeCost = 330;
 		}
 	}
-	
+
 	public String getFaculty() {
 		return faculty;
 	}
@@ -48,12 +53,12 @@ public class Course extends Tile {
 	public Student getOwner() {
 		return ownedBy;
 	}
-	
+
 	public Student setOwner(Student that) {
 		ownedBy = that;
 		return ownedBy;
 	}
-	
+
 	public int getBuyPrice() {
 		return buyPrice;
 	}
@@ -61,32 +66,32 @@ public class Course extends Tile {
 	public int getSellPrice() {
 		return sellPrice;
 	}
-	
+
 	private int getTutorialPrice() {
 		return tutorialPrice;
 	}
-	
+
 	public int getTutorialPriceOwed() {
 		return getTutorialPrice() * courseLevel;
 	}
-	
+
 	public boolean getOwnedStatus() {
 		return ownedStatus;
 	}
-	
+
 	public int getCourseLevel() {
 		return courseLevel;
 	}
-	
+
 	public int getUpgradeCost() {
 		return facultyUpgradeCost;
 	}
-	
+
 	public boolean setOwnedStatus(boolean check) {
 		this.ownedStatus = check;
 		return ownedStatus;
 	}
-	
+
 	public int addCourseLevel() {
 		if (this.courseLevel < 4) {
 			if (this.courseLevel != 1) {
@@ -97,16 +102,14 @@ public class Course extends Tile {
 		}
 		return -1;
 	}
-	
+
 	public void resetCourseLevel() {
 		this.courseLevel = 1;
 		this.facultyUpgradeCost = initialFacultyUpgradeCost;
 	}
-	
+
 	public int getTileID() {
 		return super.getTileID();
 	}
-		
-		
-}
 
+}
