7da5428cf4332f0957b7ac7572a4f9da65f6fa2a
desobob11 <67239552+desobob11@users.noreply.github.com>
2020-08-04 11:06:20 -0600

    Controller

---
+++ src/Application/MainMenuController.java
@@ -0,0 +1,35 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MainMenuController {
+
+    @FXML
+    private ImageView titleImage;
+
+    @FXML
+    private ChoiceBox<?> playersChoiceBox;
+
+    @FXML
+    private Label numOfPlayersLabel;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    void startGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitGame(ActionEvent event) {
+
+    }
+
+}
