423c3dff7b8ad9c7936016ed86dac34c3200c8e4
pi-sister <67890664+pi-sister@users.noreply.github.com>
2020-08-05 17:32:21 -0600

    Complete Chance Comments

--- src/Chance.java
+++ src/Chance.java
@@ -3,6 +3,10 @@ import java.util.Random;
 
 /**
  * This is a Chance Tile that randomly chooses something for the student to do.
+ * This is a Chance Tile, called when the player lands on the tile of type
+ * "Chance". Draws a Chance card at random and with a specific impact on the
+ * player's game.
+ * 
  * @author Arnuv Mayank
  * @author Victor Manuel Campos Goitia Campos
  *
@@ -16,6 +20,7 @@ public class Chance extends Tile {
 
 	private int randChance;
 	
+
 	/*
 	 * Creates a Tile object called Chance.
 	 * 
@@ -36,12 +41,22 @@ public class Chance extends Tile {
 
 	/*
 	 * Calls the appropriate methods to act on each chance
+	 * Calls the appropriate methods to act on each possible Chance card.
 	 * 
 	 * @param selection The number referencing which card case should be used.
+	 * 
 	 * @param studentOn The student currently invoking Chance.
+	 * 
 	 * @param allStudents The list of all students.
 	 * @param parkingTiles The list of @Tiles of type "Parking".
 	 * @param probationTile The @Tile of type "Probation".
+	 * 
+	 * @param parkingTiles The list of Tiles of type "Parking".
+	 * 
+	 * @param probationTile The Tile of type "Probation".
+	 * 
+	 * @return 1 (if action can be completed), -1 (if player has not enough money
+	 * and courses), or -2 (if player could sell courses to get enough money).
 	 */
 	public int performChanceOption(int selection, Student studentOn, ArrayList<Student> allStudents,
 			ArrayList<Parking> parkingTiles, Probation probationTile) {
@@ -55,6 +70,8 @@ public class Chance extends Tile {
 			return 1;
 		case 2:
 			int tileToMoveTo = studentOn.moveToClosestParking(parkingTiles.get(0).getTileID(), parkingTiles.get(1).getTileID());
+			int tileToMoveTo = studentOn.moveToClosestParking(parkingTiles.get(0).getTileID(),
+					parkingTiles.get(1).getTileID());
 			return parkingTiles.get(tileToMoveTo).payParkingFee(studentOn);
 		case 3:
 			return studentOn.withdrawMoney(250);
@@ -67,6 +84,21 @@ public class Chance extends Tile {
 		return 1;
 	}
 
+	/*
+	 * Invokes the performChanceOption method to perform the Tile action of the Tile
+	 * type "Chance".
+	 * 
+	 * @param student The current student invoking Chance.
+	 * 
+	 * @param students The list of all students on the board.
+	 * 
+	 * @param UI The UI object currently running.
+	 * 
+	 * @param courseList The courseList object currently handling the board info.
+	 * 
+	 * @return 1 (if action can be completed), -1 (if player has not enough money
+	 * and courses), or -2 (if player could sell courses to get enough money).
+	 */
 	@Override
 	public int performTileAction(Student student, ArrayList<Student> students, UI UI, CourseList courseList) {
 		if (!this.getPerformedTileAction()) {
@@ -74,6 +106,8 @@ public class Chance extends Tile {
 		}
 		UI.displayChanceOption(this, randChance);
 		return this.performChanceOption(randChance, student, students, courseList.getParkingTiles(), courseList.getProbation());
+		return this.performChanceOption(randChance, student, students, courseList.getParkingTiles(),
+				courseList.getProbation());
 	}
 
 }
