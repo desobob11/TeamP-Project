512f0fde345b387eaa6e28ee248d7a41a13f88d9
pi-sister <67890664+pi-sister@users.noreply.github.com>
2020-08-10 19:27:30 -0600

    Updated Probation comments

--- src/model/Probation.java
+++ src/model/Probation.java
@@ -12,15 +12,34 @@ import application.UI;
 public class Probation extends Tile {
 	private int probationCost = 250;
 	
+	/**
+	 * Constructs a new Probation with specified position.
+	 * 
+	 * @param position the desired position on the board.
+	 */
 	public Probation(int position) {
 		super(position, "Probation");
 	}
 	
+	/**
+	 * Constructs a new Probation with a specified position and a specified
+	 * probation cost.
+	 * 
+	 * @param position      the desired position on the board.
+	 * @param probationCost the desired cost of landing on Probation.
+	 */
 	public Probation(int position, int probationCost) {
 		super(position, "Probation");
 		this.probationCost = probationCost;
 	}
 	
+	/**
+	 * Removes the Probation cost from the bank of the player in Probation.
+	 * 
+	 * @param student the Student in Probation.
+	 * 
+	 * @return numerical confirmation of withdrawal from {@code withdrawMoney}
+	 */
 	private int probationPayment(Student student) {
 		//puts them in jail in case they just arrived
 		student.goToJail();
@@ -31,10 +50,26 @@ public class Probation extends Tile {
 		return student.withdrawMoney(probationCost);
 	}
 	
+	/**
+	 * Fetches the cost of landing on Probation.
+	 * 
+	 * @return cost of Probation.
+	 */
 	public int getProbationCost() {
 		return probationCost;
 	}
 
+	/**
+	 * Calls the appropriate methods to complete full Probation tile action.
+	 * 
+	 * @param student the student on the Probation tile.
+	 * @param students the list of students on the board.
+	 * @param UI the UI to modify.
+	 * @param courseList the list of tiles on the board.
+	 * 
+	 * @return the numerical confirmation of tile action: 1 (if action can be completed), -1 (if player has not enough money
+	 * and courses), or -2 (if player could sell courses to get enough money).
+	 */
 	@Override
 	public int performTileAction(Student student, ArrayList<Student> students, UI UI, CourseList courseList) {
 		// TODO Auto-generated method stub
